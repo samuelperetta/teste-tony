8b1fbc760de290f3e2dfe50824d84563a747a1d7
Samuel Peretta <samuel.peretta@gmail.com>
2018-11-29 18:03:49 -0200

    Creating structure

--- index.php
+++ index.php
@@ -1,9 +1,148 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title></title>
+    <link href="https://fonts.googleapis.com/css?family=Encode+Sans" rel="stylesheet">
 </head>
 <body>
+    <header>
+        <h1 id="logo" class="rpc">
+            <a href="#">Tony Veículos</a>
+        </h1>
+    </header>
+    <section id="banner" class="container">
+        <div class="row">
+            <div class="col-md-12 call">
+                <h2>Ficou muito mais simples ter um carro!</h2>
+                <p>Só aqui na Tony mesmo você que está com o <b>Nome Sujo</b> pode ter seu carro novo e sem burocracia. A Tony acredita em Você! </p>
+            </div>
+            <div class="col-md-12">
+                <article>
+                    <h3>Renegade 2017 - 1.8 Autom.</h3>
+                    <div class="price-from">
+                        <sub>De</sub>
+                        <h3>R$ 80.000,00</h3>
+                    </div>
+                    <div class="price-to">
+                        <sub>Por</sub>
+                        <h3>R$ 69.900,00</h3>
+                    </div>
+                    <ul>
+                        <li>1 Ano de Garantia</li>
+                        <li>Completo</li>
+                        <li>Câmbio Automático</li>
+                        <li>Roda de Liga Leve</li>
+                    </ul>
+                    <img src="">
+                </article>
+            </div>
+        </div>
+    </section>
+    <section id="contact" class="container">
+        <div class="row">   
+            <div class="col-md-12">
+                <h2>SAIBA MAIS</h2>
+                <p>Para mais informações preencha seus dados e entraremos em contato</p>
+                <form>
+                    <input type="text" name="Nome">
+                    <input type="text" name="Telefone">
+                    <select>
+                        <option value="" disabled selected>Melhor horário para contato</option>
+                        <option>Manhã</option>
+                        <option>Tarde</option>
+                        <option>Noite</option>
+                    </select>
+                    <input type="submit" class="btn" value="Enviar">
+                </form>
+        </div>
+    </section>
+    <section id="howitworks" class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Como Funciona ?</h2>
+                <p></p>
+            </div>
+            <div class="col-md-4">
+                <img>
+            </div>
+            <div class="col-md-8">
+                <p><b>A Tony vende para mim que estou com o nome sujo? </b><br/>
+                    Sim, a Tony Veículos acredita que mesmo você que está negativado tem o direito de ter um carro novo.
+                </p>
 
+                <p><b>Posso parcelar a entrada? </b><br/>
+                    Infelizmente para essas condições não podemos parcelar a entrada, porem a Tony tem um produto chamado Compra Programada que ajuda você que quer comprar um carro mas está com o nome sujo e não tem entrada.
+                    <a href="#contact">para saber mais clique aqui.</a>
+                </p>
+
+                <p><b>Não tenho CNH, posso comprar mesmo assim? </b><br/>
+                    Infelizmente é necessário ter a CNH para vendermos nessas condições.
+                </p>
+            </div>
+        </div>
+    </section>
+    <section id="faq" class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Outras Dúvidas</h2>
+                <p>Entenda as condições que criamos para você sair de carro novo</p>
+            </div>
+                <div class="col-md-8">
+                    <p><b>Como posso dar a entrada?</b><br/>
+                    Para essas condições a entrada precisa ser feita em dinheiro.</p>
+
+                    <p><b>Posso dar um carro como entrada? </b><br/>
+                    Sim, aceitamos o seu usado como entrada.</p>
+
+                    <p><b>O que a garantia de 1 ano cobre ? </b><br/>
+                    A garantia cobre: motor, câmbio e partes internas.</p>
+                </div>
+                <div class="col-md-4">
+                    <img>
+                </div>
+        </div>
+    </section>
+    <section id="2ndcall" class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Você está esperando o que para começar ?</h2>
+                <p>Prencha seus dados agora e entraremos em contato</p>
+            </div>
+        </div>
+    </section>
+    <section id="badges" class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <ul>
+                    <li>><img src=""></li>
+                    <li>><img src=""></li>
+                </ul>
+                <ul>
+                    <li>><img src=""></li>
+                    <li>><img src=""></li>
+                    <li>><img src=""></li>
+                </ul>
+            </div>
+
+        </div>
+    </section>
+    <footer>
+        <img src="">
+        <p>Todos os direitos reservados ©Tony veiculos</p>
+        <div class="social">
+            <a href=""><img src=""></a>
+            <a href=""><img src=""></a>
+            <a href=""><img src=""></a>
+        </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
 </body>
 </html>
